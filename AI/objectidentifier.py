b0cf9acd38016fa495b9f922a662bcd7a2274686
Tamer Maher <tmaher1982@gmail.com>
2023-08-21 00:18:01 +0400

    WI-31 updating logic to call the URL if the object detected is bear or dog

--- AI/objectidentifier.py
+++ AI/objectidentifier.py
@@ -6,20 +6,40 @@ import numpy as np
 from cvlib.object_detection import draw_bbox
 import concurrent.futures
  
+import webbrowser
+
+
+
 
 # Change this URL based on the ip address granted to the ESP32 cam webserver 
-#url='http://192.168.0.130/cam-hi.jpg'
+
 url='http://192.168.0.130/capture.jpg'
+alert_url = 'http://192.168.0.187/setWildStatus?s=1'
+safe_url = 'http://192.168.0.187/setWildStatus?s=0'
+animal_detected = " "
 
 im=None
- 
+
+
+# Based on the case call the alert System
+def setAlert(animal_detected):
+     
+    print("setalert is called for " + animal_detected)
+
+    if animal_detected == "bear":
+        print("It's a bear")
+        urllib.request.urlopen(alert_url)
+    elif animal_detected == "dog":
+     # if dog = false, turn off the alert system
+        urllib.request.urlopen(safe_url)
+
 def run1():
     cv2.namedWindow("live transmission", cv2.WINDOW_AUTOSIZE)
     while True:
         img_resp=urllib.request.urlopen(url)
         imgnp=np.array(bytearray(img_resp.read()),dtype=np.uint8)
         im = cv2.imdecode(imgnp,-1)
- 
+
         cv2.imshow('live transmission',im)
         key=cv2.waitKey(5)
         if key==ord('q'):
@@ -37,11 +57,17 @@ def run2():
         # This part draws a box and a label with the detected object name 
         bbox, label, conf = cv.detect_common_objects(im)
 
-        # Call the isWild and inject the label of the detecteed animal
-        isWild(label)
         im = draw_bbox(im, bbox, label, conf)
- 
+
+
+        # Call the isWild and inject the label of the detecteed animal
+        # isWild(label)
+        print(label , " detected")
+        animal_detected = label
+        setAlert(animal_detected)
+
         cv2.imshow('detection',im)
+
         key=cv2.waitKey(5)
         if key==ord('q'):
             break
@@ -53,27 +79,8 @@ def run2():
 if __name__ == '__main__':
     print("started")
     with concurrent.futures.ProcessPoolExecutor() as executer:
-            f1= executer.submit(run1)
-            f2= executer.submit(run2)
-
-
-# Define wild animal and not wild animal 
-def isWild(label):
-    safeAnimalList = ["cat", "dog", "chicken"]
-    wildAnimalList = ["beer", "lion"]
-
-    if label in wildAnimalList:
-        setAlert(true)
-    else:
-        setAlert(false)
+             f1= executer.submit(run1)
+             f2= executer.submit(run2)
 
 
 
-# Based on the case call the alert System
-def setAlert(isWild):
-     if isWild:
-     # if isWild = true , turn on the alert sytem
-        urllib.request.urlopen('http://192.168.0.187/setWildStatus?s=1')
-     else:
-     # if isWild = false, turn off the alert system
-        urllib.request.urlopen('http://192.168.0.187/setWildStatus?s=0')
