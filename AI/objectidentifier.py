adaf700c3f178da4ba46a89a6d3c0c7a21376b36
Tamer Maher <tmaher1982@gmail.com>
2023-08-20 19:39:43 +0400

    WI-31 WildSystem Logic and Integrating with Alert System

--- AI/objectidentifier.py
+++ AI/objectidentifier.py
@@ -6,7 +6,11 @@ import numpy as np
 from cvlib.object_detection import draw_bbox
 import concurrent.futures
  
-url='http://192.168.10.162/cam-hi.jpg'
+
+# Change this URL based on the ip address granted to the ESP32 cam webserver 
+url='http://192.168.0.130/cam-hi.jpg'
+
+
 im=None
  
 def run1():
@@ -29,8 +33,12 @@ def run2():
         img_resp=urllib.request.urlopen(url)
         imgnp=np.array(bytearray(img_resp.read()),dtype=np.uint8)
         im = cv2.imdecode(imgnp,-1)
- 
+
+        # This part draws a box and a label with the detected object name 
         bbox, label, conf = cv.detect_common_objects(im)
+
+        # Call the isWild and inject the label of the detecteed animal
+        isWild(label)
         im = draw_bbox(im, bbox, label, conf)
  
         cv2.imshow('detection',im)
@@ -47,3 +55,25 @@ if __name__ == '__main__':
     with concurrent.futures.ProcessPoolExecutor() as executer:
             f1= executer.submit(run1)
             f2= executer.submit(run2)
+
+
+# Define wild animal and not wild animal 
+def isWild(label):
+    safeAnimalList = ["cat", "dog", "chicken"]
+    wildAnimalList = ["beer", "lion"]
+
+    if label in wildAnimalList:
+        setAlert(true)
+    else:
+        setAlert(false)
+
+
+
+# Based on the case call the alert System
+def setAlert(isWild):
+     if isWild:
+     # if isWild = true , turn on the alert sytem
+        urllib.request.urlopen('http://192.168.0.187/setWildStatus?s=1')
+     else:
+     # if isWild = false, turn off the alert system
+        urllib.request.urlopen('http://192.168.0.187/setWildStatus?s=0')
